6a400ab9732d97f20cb8a5bb82347bcad5c3c443
Tham Dexmund <thdexmun@u81z01s02.42kl.edu.my>
2026-06-11 14:15:05 +0800

    DO THIS DEX

---
+++ ft_print_comb.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thdexmun <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/10 15:55:55 by thdexmun          #+#    #+#             */
+/*   Updated: 2026/06/10 16:07:16 by thdexmun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//note to dex: complete this and the rest. this is c00 ex05
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb(void)
+{
+	while ()
+}
